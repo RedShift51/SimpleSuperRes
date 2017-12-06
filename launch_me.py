ccc1c6a60cc84d3a5996eeed007c32723cdc3194
RedShift51 <alex_77_@mail.ru>
2017-12-07 00:32:57 +0300

    second commit

--- launch_me.py
+++ launch_me.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Dec  3 19:04:31 2017
 
-@author: alex
-"""
 
 import tensorflow as tf, numpy as np
 import scipy
@@ -32,12 +28,7 @@ class ConvLayer2D():
         ans = tf.concat([tf.nn.conv2d(x, self.weights[i][0], padding = self.padding, \
                         strides = self.strides) + self.weights[i][1] \
                         for i in range(len(self.weights))], axis = 0)
-        """
-        conv = tf.concat([tf.nn.conv2d(x, self.weights[i][0], padding = self.padding, \
-                strides = self.strides) + \
-                self.weights[i][1] for i in range(len(self.weights))],\
-                axis = 0)
-        """
+
         if self.nonlin == 'relu':
             return tf.nn.relu(ans)
         elif self.nonlin == 'sigmoid':
@@ -45,9 +36,48 @@ class ConvLayer2D():
         else:
             return tf.nn.relu(ans)
 
+
     def __call__(self, x):
         return self.transform(x)
 
+sequence_pos = [np.arange(0,24,3), np.arange(1,25,3), np.arange(2,26,3)]
+#sec_seq = np.arange(1,26,3)
+#thr_seq = np.arange(2,27,3)
+sequence_pos = [[np.meshgrid(i,j) for j in sequence_pos] for i in sequence_pos]
+np.meshgrid(fir_seq, sec_seq)    
+
+def reshaping(X):
+    
+    #nine points
+    #cond = lambda i : tf.logical_and(tf.equal(tf.truncatemod(i,3), 0), \
+    #                                 tf.less_equal(i, 25))
+    x = tf.range(0,25,3)
+    #print(x.get_shape().as_list())
+    #cond = lambda i : tf.equal(tf.truncatemod(i+1,3), 0)
+    slicing = lambda i : tf.slice(X, [0,0,i], [-1,-1,3])
+    #with tf.Session() as sess:
+    #    print(sess.run(cond([1,2,3,4,5])))
+    res = tf.map_fn(slicing, x)
+    print(res[0].get_shape().as_list())
+    """
+    X1 = tf.slice(X, [0,0,0], [-1,-1,3])
+    X2 = tf.slice(X, [0,0,3], [-1,-1,3])
+    X3 = tf.slice(X, [0,0,6], [-1,-1,3])
+    X4 = tf.slice(X, [0,0,9], [-1,-1,3])
+    X5 = tf.slice(X, [0,0,12], [-1,-1,3])
+    X6 = tf.slice(X, [0,0,15], [-1,-1,3])
+    X7 = tf.slice(X, [0,0,18], [-1,-1,3])
+    X8 = tf.slice(X, [0,0,21], [-1,-1,3])
+    X9 = tf.slice(X, [0,0,24], [-1,-1,3])
+    """
+    fir_seq = tf.range(0,25,3)
+    sec_seq = tf.range(1,26,3)
+    thr_seq = tf.range(2,27,3)
+    
+    X1 = tf.SparseTensor(indices = , values = X1, dense_shape = [24,24,3])
+    
+    
+    
 tf.reset_default_graph()
 X = tf.placeholder(dtype=tf.float32, shape=(1, 12, 12, 3))
 Y = tf.placeholder(dtype=tf.float32, shape=(1, 24, 24, 3))
@@ -56,19 +86,19 @@ Y = tf.placeholder(dtype=tf.float32, shape=(1, 24, 24, 3))
 def LayersBlock(X, scope):
     #patches learning
     X = tf.nn.sigmoid(ConvLayer2D('conv1', kernel_shape = [3,3,3,6], num_filters = 6)(X))
-    X = tf.nn.relu(batch_norm(X))
+    X = batch_norm(X)
     X = tf.nn.max_pool(X, [1,2,2,1], [1,1,1,1], 'VALID')
     print(X.get_shape().as_list())
     X = tf.nn.sigmoid(ConvLayer2D('conv2', kernel_shape = [3,3,6,12], num_filters = 10)(X))
-    X = tf.nn.relu(batch_norm(X))
+    X = batch_norm(X)
     X = tf.nn.max_pool(X, [1,2,2,1], [1,1,1,1], 'VALID')
     print(X.get_shape().as_list())
     X = tf.nn.sigmoid(ConvLayer2D('conv3', kernel_shape = [3,3,12,18], num_filters = 10)(X))
-    X = tf.nn.relu(batch_norm(X))
+    X = batch_norm(X)
     X = tf.nn.max_pool(X, [1,2,2,1], [1,1,1,1], 'VALID')
     print(X.get_shape().as_list())
     X = tf.nn.sigmoid(ConvLayer2D('conv4', kernel_shape = [3,3,18,32], num_filters = 10)(X))
-    X = tf.nn.relu(batch_norm(X))
+    X = batch_norm(X)
     X = tf.nn.max_pool(X, [1,2,2,1], [1,1,1,1], 'VALID')
     print(X.get_shape().as_list())
     X = tf.nn.sigmoid(ConvLayer2D('conv5', kernel_shape = [3,3,32,9], num_filters = 3)(X))
@@ -82,7 +112,7 @@ def LayersBlock(X, scope):
                   ], axis = 2)
     X = tf.nn.sigmoid(X)
     print(X.get_shape().as_list())
-    X = tf.reshape(X, shape = [1, 24, 24, 3])
+    X = reshaping(X)#, shape = [1, 24, 24, 3])
     return X
     
 ans = LayersBlock(X, scope='block')
@@ -128,4 +158,22 @@ with tf.Session() as sess:
         
         
         
-        
+
+
+
+
+def body(x):
+    a = tf.random_uniform(shape=[2, 2], dtype=tf.int32, maxval=100)
+    b = tf.constant(np.array([[1, 2], [3, 4]]), dtype=tf.int32)
+    c = a + b
+    return tf.nn.relu(x + c)
+
+def condition(x):
+    return tf.reduce_sum(x) < 100
+
+x = tf.Variable(tf.constant(0, shape=[2, 2]))
+
+with tf.Session():
+    tf.initialize_all_variables().run()
+    result = tf.while_loop(condition, body, [x])
+    print(result.eval())
